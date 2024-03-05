20c0807d092583085a4d94872faba61311770f7c
manrick Lázaro <manricksantos@gmail.com>
2024-03-05 11:39:22 -0300

    chore: instancia as rotas e os middlewares no servidor.(#4)

--- src/app.js
+++ src/app.js
@@ -4,6 +4,9 @@ import routes from "./routes";
 class App {
     constructor() {
         this.server = express();
+
+        this.middlewares();
+        this.routes();
     }
 
     middlewares() {
