4fa0750436fd1a1b9d91d29ada25783dd42e86a1
manrick Lázaro <manricksantos@gmail.com>
2024-04-25 10:17:55 -0300

    atualiza arquivos

--- src/app/controllers/SessionController.js
+++ src/app/controllers/SessionController.js
@@ -3,36 +3,36 @@ import User from "../models/User";
 import auth from "../../config/auth";
 
 class SessionController {
-    async store(req, res) {
-        const { email, password } = req.body;
+	async store(req, res) {
+		const { email, password } = req.body;
 
-        const user = await User.findOne({ where: { email } });
+		const user = await User.findOne({ where: { email } });
 
-        // verificação por email
-        if (!user) {
-            return res
-                .status(401)
-                .json({ error: "Não existe nenhum usuario com esse email." });
-        }
+		// verificação por email
+		if (!user) {
+			return res
+				.status(401)
+				.json({ error: "Não existe nenhum usuario com esse email." });
+		}
 
-        // verificação por senha
-        if (!(await user.checkPassword(password))) {
-            return res.status(401).json({ error: "senha inválida" });
-        }
+		// verificação por senha
+		if (!(await user.checkPassword(password))) {
+			return res.status(401).json({ error: "senha inválida" });
+		}
 
-        const { id, name } = user;
+		const { id, name } = user;
 
-        return res.json({
-            user: {
-                id,
-                name,
-                email,
-            },
-            token: jwt.sign({ id }, auth.secret, {
-                expiresIn: auth.expiresIn,
-            }),
-        });
-    }
+		return res.json({
+			user: {
+				id,
+				name,
+				email,
+			},
+			token: jwt.sign({ id }, auth.secret, {
+				expiresIn: auth.expiresIn,
+			}),
+		});
+	}
 }
 
 export default new SessionController();
