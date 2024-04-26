87cfe233c7a880576619b909bff82fa6b1c2bcfb
Manrick <77447723+Manrick-Lazaro@users.noreply.github.com>
2024-04-26 12:11:30 +0000

    update config/database.(#64)

--- src/config/database.js
+++ src/config/database.js
@@ -13,3 +13,17 @@ module.exports = {
 		underscoredAll: true, // nome relacionamento
 	},
 };
+
+module.exports = {
+	dialect: process.env.DB_DIALECT,
+	host: process.env.DB_HOST,
+	port: process.env.DB_PORT,
+	username: process.env.DB_USERNAME,
+	password: process.env.DB_PASSWORD,
+	database: process.env.DATABASE,
+	define: {
+		timestamps: true,
+		underscored: true, // nome tabele user_id
+		underscoredAll: true, // nome relacionamento
+	},
+};
