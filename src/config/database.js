611c851bf69c8dc409c2b8171a980dc6308413ea
Manrick <77447723+Manrick-Lazaro@users.noreply.github.com>
2024-04-26 12:49:08 +0000

    update Pipeline.(#64)

--- src/config/database.js
+++ src/config/database.js
@@ -14,16 +14,3 @@ module.exports = {
 	},
 };
 
-module.exports = {
-	dialect: process.env.DB_DIALECT,
-	host: process.env.DB_HOST,
-	port: process.env.DB_PORT,
-	username: process.env.DB_USERNAME,
-	password: process.env.DB_PASSWORD,
-	database: process.env.DATABASE,
-	define: {
-		timestamps: true,
-		underscored: true, // nome tabele user_id
-		underscoredAll: true, // nome relacionamento
-	},
-};
